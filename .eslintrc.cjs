cbb5194e4b31e3c0e62269ae57cb54762f4a466d
Vladimir Kurganov <err0rfrost@gmail.com>
2023-06-06 00:09:13 +0600

    init: TS-ESLint-Prettier-ARBNB config for nodeJS

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+    env: {
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'airbnb',
+        'airbnb-typescript',
+        'airbnb/hooks',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+    },
+    plugins: ['@typescript-eslint', 'prettier'],
+    rules: {},
+};
