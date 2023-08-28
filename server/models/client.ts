df7a8d03144e179e098a4bcad448777131eb759d
Vladimir Kurganov <err0rfrost@gmail.com>
2023-08-28 19:48:06 +0600

    feat: add Client schema

---
+++ server/models/client.ts
@@ -0,0 +1,65 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+const ClientSchema = new Schema(
+    {
+        clientNumber: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        firstname: {
+            type: String,
+            required: true,
+        },
+        lastname: {
+            type: String,
+            required: true,
+        },
+        sex: {
+            type: Boolean,
+            required: true,
+        },
+        birthDate: {
+            type: Date,
+            required: true,
+        },
+        phone: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: false,
+        },
+        comment: {
+            type: String,
+            required: false,
+        },
+        admin: {
+            type: Schema.Types.ObjectId,
+            ref: 'Admin',
+        },
+        clientType: {
+            type: Schema.Types.ObjectId,
+            ref: 'ClientType',
+        },
+        clientOrders: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Orders',
+            },
+        ],
+        abonement: {
+            type: Schema.Types.ObjectId,
+            ref: 'Abonement',
+        },
+        active: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model('Client', ClientSchema);
