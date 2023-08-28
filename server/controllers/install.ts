00e7017e5b8a1b28f2bce69515ca7155a07c8031
Vladimir Kurganov <err0rfrost@gmail.com>
2023-08-28 19:49:34 +0600

    feat: install Abonement and clientType schemas to db

--- server/controllers/install.ts
+++ server/controllers/install.ts
@@ -1,5 +1,7 @@
 import { Response, Request } from 'express';
 import Roles from '../models/roles.js';
+import Abonement from '../models/abonement.js';
+import ClientType from '../models/clientType.js';
 
 const installRoles = async () => {
     try {
@@ -20,10 +22,50 @@ const installRoles = async () => {
     }
 };
 
+const installClientTypes = async () => {
+    try {
+        const clientTypesData = [
+            { type: 'standart', discount: 0 },
+            { type: 'svoi', discount: 100 },
+        ];
+        await ClientType.insertMany(clientTypesData);
+
+        return {
+            message: 'Инсталляция ClientTypes прошла успешно',
+        };
+    } catch (error) {
+        return {
+            message: 'Инсталляция ClientTypes прошла с ошибкой',
+        };
+    }
+};
+
+const installAbonement = async () => {
+    try {
+        const abonementData = [
+            { abonement: 'Разовый' },
+            { abonement: '4 занятия' },
+            { abonement: '8 занятий' },
+            { abonement: '12 занятий' },
+        ];
+        await Abonement.insertMany(abonementData);
+
+        return {
+            message: 'Инсталляция Abonements прошла успешно',
+        };
+    } catch (error) {
+        return {
+            message: 'Инсталляция Abonements прошла с ошибкой',
+        };
+    }
+};
+
 // Install DB
 export const install = async (req: Request, res: Response) => {
     try {
         await installRoles();
+        await installAbonement();
+        await installClientTypes();
         return res.json({
             message: 'Инсталляция DB прошла успешно',
         });
