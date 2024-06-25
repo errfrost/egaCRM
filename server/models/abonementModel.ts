e3cbc82779155b103874916e16f9d0c5c9018e9c
Vladimir Kurganov <err0rfrost@gmail.com>
2024-06-25 16:26:10 +0600

    feat: abonement model

--- server/models/abonementModel.ts
+++ server/models/abonementModel.ts
@@ -3,10 +3,32 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 const AbonementSchema = new Schema(
     {
-        abonement: {
+        clientNumber: {
             type: String,
             required: true,
             unique: true,
+        },
+        abonementType: {
+            type: Schema.Types.ObjectId,
+            ref: 'AbonementType',
+            required: true,
+        },
+        startDate: {
+            type: Date,
+            required: true,
+        },
+        endDate: {
+            type: Date,
+            required: true,
+        },
+        activeLessons: {
+            type: Number,
+            required: true,
+        },
+        order: {
+            type: Schema.Types.ObjectId,
+            ref: 'Order',
+            required: true,
         },
     },
     { timestamps: true }
