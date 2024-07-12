20001fc0804bdcd4d5bad788fc67858215257822
Vladimir Kurganov <err0rfrost@gmail.com>
2024-07-12 18:33:12 +0600

    fix: type in TeacherSchema

--- server/models/teacherModel.ts
+++ server/models/teacherModel.ts
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const TeacherSchema = new Schema(
     {
         teacherNumber: {
-            type: String,
+            type: Number,
             required: true,
             unique: true,
         },
